d8f3d592be44fdb45878e19d4db7f46443166bb7
Farina Alex <raistnemiss@gmail.com>
2026-05-08 14:11:36 +0200

    fix: use JSON file path instead of dict for emmiter enrichment

--- src/enrich.py
+++ src/enrich.py
@@ -1,23 +1,47 @@
+import json
 from pathlib import Path
 from collections import Counter
 
 
-def candidats_fréquents(logs: list[dict]) -> list[tuple[str, int]]:
+def candidats_frequents(logs: list[dict]) -> list[tuple[str, int]]:
 
     emetteurs = []
-    seuil_occurrence = 5  # seuil de fréquence pour considérer un candidat comme pertinent
+    seuil_occurrence = (
+        5  # seuil de fréquence pour considérer un candidat comme pertinent
+    )
 
     for log in logs:
-            emetteurs.extend(log.get("emetteur_candidats", []))
+        emetteurs.extend(log.get("emetteur_candidats", []))
 
     counter = Counter(emetteurs)
-    
-    return [
-        (emetteur, occurrence) 
-        for emetteur, occurrence in counter.most_common() 
-        if occurrence >= seuil_occurrence]
 
-def ajouter_emetteur_json(emetteur_select: str, categorie_emetteur: str, emetteur_json_path: Path):
-      nouveau_emetteur = emetteur_select.strip()
-      print(nouveau_emetteur)
-      
+    return [
+        (emetteur, occurrence)
+        for emetteur, occurrence in counter.most_common()
+        if occurrence >= seuil_occurrence
+    ]
+
+
+def ajouter_emetteur_json(emetteur_select: str, categorie_emetteur: str, emetteur_json_path: Path) -> None:
+    
+    nouveau_emetteur = emetteur_select.strip()
+    nouvelle_clé_emetteur = (nouveau_emetteur.lower().replace(" ", "_"))
+
+
+    nouvelle_entree = {
+        "description": nouveau_emetteur,
+        "category": categorie_emetteur,
+        "mot_clef": {
+            nouveau_emetteur.lower(): 5,
+            },
+        }
+
+    with open(emetteur_json_path, "r", encoding="utf-8") as f:
+        data_emetteurs = json.load(f)
+
+    data_emetteurs[nouvelle_clé_emetteur] = nouvelle_entree
+
+    with open(emetteur_json_path, "w", encoding="utf-8") as f:
+        json.dump(data_emetteurs, f, indent=4, ensure_ascii=False)
+    
+    print(f"✅ Émetteur ajouté : {nouvelle_clé_emetteur}")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-""" PanDaFire - A simple automatic PDF rename script by Raist Nemiss."""
+"""PanDaFire - A simple automatic PDF rename script by Raist Nemiss."""
 
 import typer
 import json
 from pathlib import Path
 
-from .extraction import extraire_texte, extraire_date_document, extraire_candidats_emetteur
+from .extraction import (
+    extraire_texte,
+    extraire_date_document,
+    extraire_candidats_emetteur,
+)
 from .classifier import identifier_par_score
 from .logger import log_decision, lire_log
-from .enrich import candidats_fréquents, ajouter_emetteur_json
+from .enrich import candidats_frequents, ajouter_emetteur_json
 
 app = typer.Typer()
 
@@ -24,35 +28,56 @@ with open(CONFIG_PATH / "emetteurs.json", encoding="utf-8") as f:
 @app.command()
 def run(
     pdf_path: Path = typer.Argument(..., help="Chemin vers le fichier PDF à traiter"),
-    debug: bool = typer.Option(False, "--debug", "-d", help="Afficher les scores de classification pour le type de document et l'émetteur"),
-    dry_run: bool = typer.Option(True, "--dry-run", "-n", help="Afficher le nouveau nom sans renommer le fichier")
-    ):
+    debug: bool = typer.Option(
+        False,
+        "--debug",
+        "-d",
+        help="Afficher les scores de classification pour le type de document et l'émetteur",
+    ),
+    dry_run: bool = typer.Option(
+        True, "--dry-run", "-n", help="Afficher le nouveau nom sans renommer le fichier"
+    ),
+):
 
     texte, ocr_utilise = extraire_texte(pdf_path)
 
     type_doc, type_doc_scores = identifier_par_score(texte, TYPES, retour_score=True)
     date_doc = extraire_date_document(texte)
-    emetteur, emetteur_scores = identifier_par_score(texte, EMETTEURS, retour_score=True)
+    emetteur, emetteur_scores = identifier_par_score(
+        texte, EMETTEURS, retour_score=True
+    )
 
-    if emetteur == "inconnu": # si l'émetteur n'est pas identifié avec suffisamment de confiance, on extrait des candidats potentiels pour analyse manuelle
+    if (
+        emetteur == "inconnu"
+    ):  # si l'émetteur n'est pas identifié avec suffisamment de confiance, on extrait des candidats potentiels pour analyse manuelle
         candidats_emetteur = extraire_candidats_emetteur(texte)
     else:
         candidats_emetteur = []
 
+    log_decision(
+        pdf_path,
+        type_doc,
+        type_doc_scores,
+        emetteur,
+        emetteur_scores,
+        candidats_emetteur,
+        date_doc,
+        ocr_utilise,
+    )
 
-    log_decision(pdf_path, type_doc, type_doc_scores, emetteur, emetteur_scores, candidats_emetteur, date_doc, ocr_utilise)
+    print(
+        f"Le document {pdf_path.name} est identifié comme : {type_doc} - Emetteur du document : {emetteur} - Date extraite : {date_doc}"
+    )
 
-    print(f"Le document {pdf_path.name} est identifié comme : {type_doc} - Emetteur du document : {emetteur} - Date extraite : {date_doc}")
 
 @app.command()
 def enrich():
-    candidats = candidats_fréquents(lire_log())
-    
+    candidats = candidats_frequents(lire_log())
+
     # Récupère la liste des catégories uniques (sans doublons)
     categories = [emetteur["category"] for emetteur in EMETTEURS.values()]
     categorie_emetteurs = list(dict.fromkeys(categories))
 
-    
     typer.echo("Candidats émetteurs fréquents :")
     for i, (nom, occurrence) in enumerate(candidats, start=1):
         typer.echo(f"{i}  - {nom} : {occurrence} occurrences")
@@ -62,21 +87,25 @@ def enrich():
     if choix == 0:
         typer.echo("Aucun candidat sélectionné.")
         return
-      
+
     candidat_select = candidats[choix - 1]
     typer.echo(f"Candidat sélectionné : {candidat_select}")
-    typer.echo(25*"-")
+    typer.echo(25 * "-")
     for i, (categorie) in enumerate(categorie_emetteurs, start=1):
         typer.echo(f"{i} - {categorie}")
-    
-    choix = typer.prompt(f"Choisir une catégorie pour le candidat : {candidat_select[0]} (0 pour quitter)", type=int)
+
+    choix = typer.prompt(
+        f"Choisir une catégorie pour le candidat : {candidat_select[0]} (0 pour quitter)",
+        type=int,
+    )
 
     if choix == 0:
         typer.echo("Aucune catégorie sélectionnée.")
-        return
-    
-    emetteur_select = categorie_emetteurs[choix-1]
-    ajouter_emetteur_json(candidat_select[0], emetteur_select, EMETTEURS)
- 
+        emmetteur_select = "générique"
+
+    emetteur_select = categorie_emetteurs[choix - 1]
+    ajouter_emetteur_json(candidat_select[0], emetteur_select, CONFIG_PATH / "emetteurs.json")
+
+
 if __name__ == "__main__":
     app()
