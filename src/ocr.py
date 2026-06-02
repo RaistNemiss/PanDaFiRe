0173d467b5f9039259c2c52b76c190bd430e5a28
RaistNemiss <raistnemiss@gmail.com>
2026-06-02 22:11:01 +0200

    refactor: update OCR functions to use PyMuPDF and rename for clarity

--- src/ocr.py
+++ src/ocr.py
@@ -2,13 +2,15 @@ import pytesseract
 from pdf2image import convert_from_path
 from pathlib import Path
 import pymupdf
+import io
+from PIL import Image
 
 from .config_path import POPPLER_PATH, TESSERACT_PATH
 
 if TESSERACT_PATH.exists():
     pytesseract.pytesseract.tesseract_cmd = str(TESSERACT_PATH)
 
-def extraire_texte_ocr(pdf_path: Path) -> str:
+def extraire_texte_ocr_pdf2image(pdf_path: Path) -> str:
 
     poppler = POPPLER_PATH if POPPLER_PATH.exists() else None # None = utiliser le PATH système
 
@@ -26,3 +28,6 @@ def extraire_texte_ocr(pdf_path: Path) -> str:
         texte += pytesseract.image_to_string(page, lang="fra+eng", config="--oem 1 --psm 6") # configuration pour une meilleure reconnaissance de texte dans les documents structurés
 
     return texte
+
+def extrire_texte_ocr_pymupdf(pdf_path: Path) -> str:
+    pass
